1126ae3938200f963c1445010805611d250707cb
VicoD3X <vicaubry.pro@gmail.com>
2025-10-01 01:03:00 +0200

    mise en place du streamlit fonctionnel

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import pickle
-from pathlib import Path
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import pickle
+# from pathlib import Path
 
-# Charger le modèle
-model_path = Path("models/trained_model.pkl")
-with open(model_path, "rb") as f:
-    model = pickle.load(f)
+# # Charger le modèle
+# model_path = Path("models/trained_model.pkl")
+# with open(model_path, "rb") as f:
+#     model = pickle.load(f)
 
-# Créer l'app FastAPI
-app = FastAPI(title="API Air Paradis - Sentiment Analysis")
+# # Créer l'app FastAPI
+# app = FastAPI(title="API Air Paradis - Sentiment Analysis")
 
-# Définir la structure de la requête
-class TweetInput(BaseModel):
-    text: str
+# # Définir la structure de la requête
+# class TweetInput(BaseModel):
+#     text: str
 
-# Route de test
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenue dans l'API Air Paradis"}
+# # Route de test
+# @app.get("/")
+# def read_root():
+#     return {"message": "Bienvenue dans l'API Air Paradis"}
 
-# Route de prédiction
-@app.post("/predict")
-def predict_sentiment(input_data: TweetInput):
-    prediction = model.predict([input_data.text])[0]
-    sentiment = "Negatif" if prediction == 1 else "Positif/Neutre"
-    return {"tweet": input_data.text, "prediction": int(prediction), "sentiment": sentiment}
+# # Route de prédiction
+# @app.post("/predict")
+# def predict_sentiment(input_data: TweetInput):
+#     prediction = model.predict([input_data.text])[0]
+#     sentiment = "Negatif" if prediction == 1 else "Positif/Neutre"
+#     return {"tweet": input_data.text, "prediction": int(prediction), "sentiment": sentiment}
