7149698ecc0e8f51e52ba3f5a68f4093ec666ae3
VicoD3X <vicaubry.pro@gmail.com>
2025-10-14 13:23:48 +0200

    maj minore

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 @st.cache_resource
 def load_model():
-    model_path = Path("models/trained_model.pkl")
+    model_path = Path("models/tfidf_vectorizer.pkl")
     with open(model_path, "rb") as f:
         model = pickle.load(f)
     return model
